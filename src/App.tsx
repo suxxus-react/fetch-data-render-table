60681ffb78766737e1668721270cfa238b383e5d
d <xx@x.com>
2022-12-20 18:14:36 +0100

    Update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import { getDeliveries, Delivery } from "./network";
+import { Dna } from "react-loader-spinner";
 
+function sortDelivery(list: Delivery[]): Delivery[] {
+  return ["active", "upcoming", "pending"]
+    .map((s) => list.filter((obj) => obj.status === s))
+    .map((xs) => xs.sort((a, b) => ((a?.eta ?? 0) > (b?.eta ?? 0) ? 1 : -1)))
+    .flat();
+}
+
+// components
+// ----------
+//
 function App() {
-  const [count, setCount] = useState(0)
+  //
+  const [delivery, setDelivery] = useState<Delivery[]>([]);
+  const isVisible = delivery.length > 0;
+
+  useEffect(() => {
+    const fetchDeliveries = async () => {
+      const data = await getDeliveries();
+      setDelivery(sortDelivery(data));
+    };
+
+    fetchDeliveries();
+  }, []);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {isVisible ? (
+        <table className="fade-in-table">
+          <thead>
+            <tr>
+              <td>id</td>
+              <td>Name</td>
+              <td>Amount</td>
+              <td>Status</td>
+              <td>Eta</td>
+            </tr>
+          </thead>
+          <tbody>
+            {delivery.map((o) => (
+              <tr key={`id_${o.id}`}>
+                <td>{o.id}</td>
+                <td>{o.name}</td>
+                <td>{o.amount}</td>
+                <td>{o.status}</td>
+                <td>{o?.eta || ""}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <Dna
+          height="80"
+          width="80"
+          ariaLabel="dna-loading"
+          wrapperStyle={{}}
+          wrapperClass="dna-wrapper"
+        />
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
